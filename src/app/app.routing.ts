7dc06fa06ef72615740d07f73a886b0a014624f5
MKudla <miroslaw.kudla@external.t-mobile.pl>
2020-04-29 00:23:20 +0200

    init

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { TestComponent } from './test';
+import { AuthGuard } from './_helpers';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'test', component: TestComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
